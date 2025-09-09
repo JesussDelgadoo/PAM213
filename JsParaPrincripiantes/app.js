fda1bfd82c166d9393445c23ab2be6de921e410a
JesussDelgadoo <122041657@upq.edu.mx>
2025-09-09 15:35:05 -0600

    Js Para Principiantes

---
+++ JsParaPrincripiantes/app.js
@@ -0,0 +1,41 @@
+// // Tipos de Datos
+// "Hello World" // String
+// 'Hello World' // String
+
+// 1000000 // Number
+// -2.3 // Number
+
+// true // Boolean
+// false // Boolean
+
+// // Array
+// ["Joe", "Ryan", "Martha"]
+// [1, 2, 3]
+// [true, false, true, false]
+
+// // Object
+
+// {
+//     "username": "Ryan",
+//     "score": 70.4,
+//     "hours": 14,
+//     "proffesional": true
+// }
+
+// {
+//     "username": "Joe",
+//     "score": 0.4,
+//     "hours": 1,
+//     "proffesional": true
+// }
+
+
+console.log("This is a string");
+console.log(14.3);
+console.log(true);
+console.log([1, 2, 3, 4]);
+console.log({"username": "Ryan", "score": 70.4});
+
+var nameuser =  "John";
+let lastname = "Carter";
+console.log(lastname);
