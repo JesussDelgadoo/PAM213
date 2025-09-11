b1c130be56d9221026fc2c21740ca48680bc8a35
JesussDelgadoo <122041657@upq.edu.mx>
2025-09-10 19:11:13 -0600

    Js Para Principiante Avance 1

--- JsParaPrincripiantes/app.js
+++ JsParaPrincripiantes/app.js
@@ -38,4 +38,75 @@ console.log({"username": "Ryan", "score": 70.4});
 
 var nameuser =  "John";
 let lastname = "Carter";
-console.log(lastname);
+
+nameuser = "Pepe";
+
+//let PI = 3.1415;
+
+const PI = 3.1415;
+// let id = "asdasdas";
+// id = "123";
+
+// console.log(lastname);
+
+console.log(PI)
+
+// username = "fazt";
+// let nickname = "fazt";
+
+// let userName = "gordon";
+
+// nombreDepersona
+
+//camelcase
+// let userName = "gordon";
+
+
+// let numberOne = 100;
+// let numberTwo = 500;
+// let result = numberOne > numberTwo;
+// console.log(result);
+
+let password = "finn1234";
+let input = "finn1234";
+let result2 = input == password;
+console.log(result);
+
+// Condicionales
+
+if (result === true) {
+    console.log("Login Correcto");
+} else {
+    console.log("Contraseña Incorrecta");
+}
+
+
+// if (result === false) {
+//     console.log("Login Incorrecto");
+// }
+
+// let score = 70;
+
+// if (score > 30) {
+//     console.log("You need to practice more");
+// } 
+
+// if (score > 15) {
+//     console.log("Estas mejorando");
+// }
+
+// else {
+//     console.log("You need to follow this tutorial");
+// }
+
+let typeCard = "Creebit Card";
+
+switch (typeCard) {
+    case 'Debit card':
+        console.log("This is a debit card");
+        break;
+    case 'Credit card':
+        console.log("This is a credit card");
+        break;
+}
+
