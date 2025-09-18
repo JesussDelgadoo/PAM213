0462571a9f8711f5465260863e952c7bcd566fca
JesussDelgadoo <122041657@upq.edu.mx>
2025-09-17 20:21:58 -0600

    Ejercicio B

---
+++ Practica1/ejercicio_b.js
@@ -0,0 +1,12 @@
+// Ejercicio B
+// function cuadrado(numero) {
+//     return numero * numero;
+//}
+
+//Corregido
+
+const cuadrado = (numero) => numero * numero;
+
+console.log("Resultado 1: " + cuadrado(2));
+console.log("Resultado 2: " + cuadrado(3));
+console.log("Resultado 3: " + cuadrado(5));
