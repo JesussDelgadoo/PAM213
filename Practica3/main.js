4bc343518824fd027ca09488ee0610bce381ef69
JesussDelgadoo <122041657@upq.edu.mx>
2025-09-25 16:59:43 -0600

    Correcion Ejercicio A

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,5 +1,5 @@
 import { restar } from "./util.js";
 
 console.log(restar(10 , 8)); // 10 - 8 = 2
-console.log(restar(10 , 8)); // 6 - 2 = 4
+console.log(restar(6 , 2)); // 6 - 2 = 4
 console.log(restar(5 , 2)); // 5 - 2 = 3
