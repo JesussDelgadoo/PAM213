ec80ca4e28940ef5d41b61367c6452d0d7ba2fbc
JesussDelgadoo <122041657@upq.edu.mx>
2025-10-08 20:15:49 -0600

    Contador

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,28 @@
+//1.- Imports: Zona de Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
+//2.- Main: Zona de Componentes
 export default function App() {
+
+  const [contador, setcontador]= useState(0);
+
   return (
+
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!! </Text>
+      
+      <Text>Contador: {contador} </Text>
+      <Button title="Agregar" onPress={()=>setcontador(contador+1)}/>
+      <Button title="Quitar" onPress={()=>setcontador(contador-1)}/>
+      <Button title="Reiniciar" onPress={()=>setcontador(contador*0)}/>
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+//3.- Styles: Zona de Estilos / Estica y Posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
