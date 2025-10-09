f65ac6762bbb1d2815c75362e05c260965300ef5
JesussDelgadoo <122041657@upq.edu.mx>
2025-10-09 15:37:41 -0600

    Estilizacion de la screen

--- intro/App.js
+++ intro/App.js
@@ -1,24 +1,14 @@
 //1.- Imports: Zona de Importaciones
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import React,{useState} from 'react';
+import ContadorScreen from './screens/ContadorScreen';
 
 //2.- Main: Zona de Componentes
 export default function App() {
 
-  const [contador, setcontador]= useState(0);
-
   return (
 
-    <View style={styles.container}>
-      
-      <Text>Contador: {contador} </Text>
-      <Button title="Agregar" onPress={()=>setcontador(contador+1)}/>
-      <Button title="Quitar" onPress={()=>setcontador(contador-1)}/>
-      <Button title="Reiniciar" onPress={()=>setcontador(contador*0)}/>
-      <StatusBar style="auto" />
-
-    </View>
+    <ContadorScreen></ContadorScreen>
   );
 }
 
