7d672b397e882f2043779aee9eb6642e8e351d08
JesussDelgadoo <122041657@upq.edu.mx>
2025-10-15 20:23:20 -0600

    Menu de todas las practicas

--- intro/App.js
+++ intro/App.js
@@ -1,23 +1,11 @@
-//1.- Imports: Zona de Importaciones
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import ContadorScreen from './screens/ContadorScreen';
 
-//2.- Main: Zona de Componentes
+import MenuScreen from './screens/MenuScreen';
+
 export default function App() {
 
   return (
-
-    <ContadorScreen></ContadorScreen>
+    <MenuScreen></MenuScreen>
   );
 }
 
-//3.- Styles: Zona de Estilos / Estica y Posicionamiento
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
